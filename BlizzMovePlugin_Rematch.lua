dd2607bdcb26c33f81a9d362254d5e08a96512f7
Mark W <numedain@gmail.com>
2024-03-03 12:49:01 +0100

    Updated for Rematch's rewrite (closes #2)

--- BlizzMovePlugin_Rematch.lua
+++ BlizzMovePlugin_Rematch.lua
@@ -9,6 +9,9 @@ end
 function Plugin:ADDON_LOADED(addonName)
     if addonName ~= name then return; end
     local compatible = false;
+
+    --- @type BlizzMoveAPI
+    local BlizzMoveAPI = _G.BlizzMoveAPI;
     if(BlizzMoveAPI and BlizzMoveAPI.GetVersion and BlizzMoveAPI.RegisterAddOnFrames) then
         local _, _, _, _, versionInt = BlizzMoveAPI:GetVersion();
         if (versionInt == nil or versionInt >= 30200) then
@@ -21,26 +24,54 @@ function Plugin:ADDON_LOADED(addonName)
         return;
     end
 
+    local subFrames;
+    if RematchJournal then
+        subFrames = {
+            RematchJournal = {
+                MinVersion = 0,
+                SubFrames = {
+                    ['RematchTeamPanel.List.ScrollFrame.CaptureButton'] = {
+                        MinVersion = 0,
+                    },
+                },
+            },
+        };
+    elseif RematchFrame then
+        subFrames = {
+            RematchFrame = {
+                MinVersion = 0,
+                SubFrames = {
+                    ['RematchFrame.TeamsPanel.List.ScrollBox'] = {
+                        MinVersion = 0,
+                    },
+                    ['RematchFrame.TargetsPanel.List.ScrollBox'] = {
+                        MinVersion = 0,
+                    },
+                    ['RematchFrame.QueuePanel.List.ScrollBox'] = {
+                        MinVersion = 0,
+                    },
+                    ['RematchFrame.OptionsPanel.List.ScrollBox'] = {
+                        MinVersion = 0,
+                    },
+                    ['RematchFrame.PetsPanel.List.ScrollBox'] = {
+                        MinVersion = 0,
+                    },
+                },
+            },
+        };
+    end
+
+    if not subFrames then
+        print(name .. ' current version of Rematch is not supported by this plugin. Please notify the author of this plugin on CurseForge or GitHub.');
+        return;
+    end
     local frameTable = {
         ['Blizzard_Collections'] =
         {
             ['CollectionsJournal'] =
             {
                 MinVersion = 20000,
-                SubFrames =
-                {
-                    ['RematchJournal'] =
-                    {
-                        MinVersion = 20000,
-                        SubFrames =
-                        {
-                            ['RematchTeamPanel.List.ScrollFrame.CaptureButton'] =
-                            {
-                                MinVersion = 20000,
-                            },
-                        },
-                    },
-                },
+                SubFrames = subFrames,
             },
         },
     };
